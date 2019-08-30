aef4789abbf37008e29ba36e4fc44bf2be17d999
sergkryk <41703448+sergkryk@users.noreply.github.com>
2019-08-30 21:30:34 +0300

    исправления после первой проверки

--- source/js/script.js
+++ source/js/script.js
@@ -7,9 +7,11 @@ var box = document.querySelector('.example__image--before');
 var beforeButton = document.querySelector('.example__toggle-btn--before');
 var afterButton = document.querySelector('.example__toggle-btn--after');
 var barIndicator = document.querySelector('.example__toggle-bar');
+var addressMap = document.querySelector('.contacts__iframe-map');
 
 navMain.classList.remove('site-header__nav--nojs');
 navMenu.classList.remove('visually-hidden');
+addressMap.classList.remove('contacts__iframe-map--nojs');
 
 navMenu.addEventListener('click', function () {
   if (navMain.classList.contains('site-header__nav--closed')) {
